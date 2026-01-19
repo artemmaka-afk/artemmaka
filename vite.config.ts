8f720acca7d9a0e8ac6bf63c657175edbb99d4c3
artemmaka-afk <artemmaka@gmail.com>
2026-01-19 16:35:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/artemmaka/',  // <-- ЭТА СТРОКА ДОБАВЛЕНА для GitHub Pages
   server: {
     host: "::",
     port: 8080,
